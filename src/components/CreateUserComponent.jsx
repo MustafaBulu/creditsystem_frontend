0d34d399eaf54b10b9708892066f4d7a54390d55
mustafabulu <you@example.com>
2022-02-27 17:45:15 +0300

    revize

--- src/components/CreateUserComponent.jsx
+++ src/components/CreateUserComponent.jsx
@@ -24,30 +24,18 @@ class CreateUserComponent extends Component {
     // step 3
     componentDidMount(){
 
-        // step 4
-        if(this.state.id === 'basvur'){
-            return
-        }else{
-            UserService.getUserById(this.state.id).then( (res) =>{
-                let user = res.data;
-                this.setState({identificationNumber: user.identificationNumber,
-                    firstName_lastName: user.firstName_lastName,
-                    monthlyIncome : user.monthlyIncome,
-                    phoneNumber : user.phoneNumber
-                });
-            });
-        }        
+           
     }
     saveUser = (e) => {
         e.preventDefault();
         let user = {identificationNumber: this.state.identificationNumber, firstName_lastName: this.state.firstName_lastName, monthlyIncome: this.state.monthlyIncome, phoneNumber: this.state.phoneNumber};
         console.log('user => ' + JSON.stringify(user));
 
-
+    
         // step 5
 
             UserService.createUser(user).then(res =>{
-                this.props.history.push('/users');
+                this.props.history.push('/krediler/basvur');
                 
                 
             });
@@ -69,11 +57,6 @@ class CreateUserComponent extends Component {
       this.setState({phoneNumber: event.target.value});
   }
 
-    getTitle(){
-        
-            return <h3 className="text-center">Hemen Bankamızdan Kredi Alın</h3>
-        
-    }
     render() {
         return (
             <div>
@@ -81,32 +64,33 @@ class CreateUserComponent extends Component {
                    <div className = "container">
                         <div className = "row">
                             <div className = "card col-md-6 offset-md-3 offset-md-3">
-                            <h3 className="text-center">Hemen Bankamızdan Kredi Alın</h3>
+                            <h3 className="text-center">Hemen Kredi Alın</h3>
                                 <div className = "card-body">
                                     <form>
                                         <div className = "form-outline mb-4">
                                             <label> Tc kimlik numaraniz: </label>
-                                            <input placeholder="Lütfen Tcnizi giriniz" name="identificationNumber" className="form-control" 
+                                            <input type="number" placeholder="Lütfen Tcnizi giriniz" name="identificationNumber" className="form-control" 
                                                 value={this.state.identificationNumber} onChange={this.changeidentificationNumberHandler}/>
                                         </div>
                                         <div className = "form-outline mb-4">
                                             <label> Adınız Soyadınız: </label>
-                                            <input placeholder="Adınız Soyadınızı giriniz" name="firstName_lastName" className="form-control" 
+                                            <input type="text" placeholder="Adınız Soyadınızı giriniz" name="firstName_lastName" className="form-control" 
                                                 value={this.state.firstName_lastName} onChange={this.changefirstName_lastNameHandler}/>
                                         </div>
                                         <div className = "form-outline mb-4">
                                             <label> Aylık gelir: </label>
-                                            <input placeholder="Aylık gelirinizi giriniz" name="monthlyIncome" className="form-control" 
+                                            <input type="number" placeholder="Aylık gelirinizi giriniz" name="monthlyIncome" className="form-control" 
                                                 value={this.state.monthlyIncome} onChange={this.changemonthlyIncomeHandler}/>
                                         </div>
                                         <div className = "form-outline mb-4">
                                             <label> Telefon Numarası: </label>
-                                            <input placeholder="Telefon Numaranızı girin" name="phoneNumber" className="form-control" 
+                                            <input type="number" placeholder="Telefon Numaranızı girin" name="phoneNumber" className="form-control" 
                                                 value={this.state.phoneNumber} onChange={this.changephoneNumberHandler}/>
                                         </div>
 
                                         <button className="btn btn-success" onClick={this.saveUser}>Kredi Başvurusu Yap</button>
                                     </form>
+                                    <p>{this.changeidentificationNumberHandler}</p>
                                 </div>
                             </div>
                         </div>
