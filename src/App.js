f5a9519c5aa5b2f6f633971d109b09f1f122d162
mustafabulu <you@example.com>
2022-02-27 17:44:05 +0300

    Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HeaderComponent from "./components/HeaderComponent";
+import CreateUserComponent from "./components/CreateUserComponent";
+import Home from "./components/Home";
+import FooterComponent from "./components/FooterComponent";
+
+// non-stateless:durumsuz yani bir yere değer döndermeyen:void
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+
+        <div className="container">
+          <Switch>
+          <Route path="/" exact component={Home}></Route>
+            <Route path="/kredibasvurusu"  component={CreateUserComponent}></Route>
+            
+          </Switch>
+        </div>
+        <FooterComponent/>
+      </Router>
     </div>
   );
 }
