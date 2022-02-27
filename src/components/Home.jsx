c8567d2dcd3ab49737082283b938e90a2651cc5b
mustafabulu <you@example.com>
2022-02-27 17:46:29 +0300

    Home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Bilgilerinizi Girerek Kolaylıkla Kredi Alabilirsiniz</h2>
+                
+                <p>Adınızı, soyadınızı, tc kimlik numaranızı ve telefon numaranızı girerek bankamız tarafından kredi alabilirsiniz.Eğer kredi almak istiyorsanız tıklayınız.</p>
+                <h2>Bilgilerinizi Girerek Kolaylıkla Kredi Alabilirsiniz</h2>
+
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
